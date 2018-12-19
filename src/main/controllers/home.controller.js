4a210fa02766332b4db9de9323b577b89a2cfa20
carlos22ivan <carlos22ivan@gmail.com>
2018-12-19 01:41:09 -0500

    added qatar and kaskada

--- src/main/controllers/home.controller.js
+++ src/main/controllers/home.controller.js
@@ -1,15 +1,28 @@
 exports.homePage = (req, res) => {
-        res.render('user/index')
+        res.render('user/index', {visitas: getVisitas()})
+}
+
+var visitas = 62
+
+function getVisitas() {
+        return ++visitas;
 }
 
 exports.reservarPage = (req, res) => {
-        res.render('user/reservar')
+        res.render('user/reservar', {visitas: getVisitas()})
 }
 
 exports.motel1 = (req, res) => {
-        res.render('user/motel1')
+        res.render('user/motel1', {visitas: getVisitas()})
 }
 
 exports.motel2 = (req, res) => {
-        res.render('user/motel2')
+        res.render('user/motel2', {visitas: getVisitas()})
+}
+
+exports.motel3 = (req, res) => {
+        res.render('user/motel3', {visitas: getVisitas()})
+}
+exports.motel4 = (req, res) => {
+        res.render('user/motel4', {visitas: getVisitas()})
 }
--- src/main/router.js
+++ src/main/router.js
@@ -6,5 +6,8 @@ router.route('/').get(homeController.homePage)
 router.route('/reservar').get(homeController.reservarPage)
 router.route('/el-castillo').get(homeController.motel1)
 router.route('/eclipse').get(homeController.motel2)
+router.route('/qatar').get(homeController.motel3)
+router.route('/la-kaskada').get(homeController.motel4)
+router.route('/*').get(homeController.homePage)
 
 module.exports = router
